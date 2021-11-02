c612156333aa56611ce4364b606a46af35b0701e
jooprima <jooprima@gmail.com>
2021-11-02 23:38:06 +0700

    Container component

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -1,18 +1,21 @@
 import Image from "next/image";
+import Container from "components/Container";
 import traveloLogo from "public/assets/travelo-logo-header.png";
 
 export const Header = () => {
   return (
     <header className="h-[85px] bg-white shadow-lg p-4">
-      <div className="flex justify-between items-center">
-        <Image
-          src={traveloLogo}
-          alt="Travelo Logo"
-          placeholder="blur"
-          height="48"
-          width="182"
-        />
-      </div>
+      <Container>
+        <div className="flex justify-between items-center">
+          <Image
+            src={traveloLogo}
+            alt="Travelo Logo"
+            placeholder="blur"
+            height="48"
+            width="182"
+          />
+        </div>
+      </Container>
     </header>
   );
 };
