0532494952de9fc99beb36596a03cc809ee7887a
jooprima <jooprima@gmail.com>
2021-11-16 00:06:24 +0700

    SecondSection component

--- src/containers/Home/SecondSection/index.tsx
+++ src/containers/Home/SecondSection/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 
+import Container from 'components/Container';
+import styles from './SecondSection.module.css'
+
 const SecondSection = () => {
     return (
-        <div>
-            
-        </div>
+        <section className={styles.section}>
+            <Container>
+                
+            </Container>
+        </section>
     )
 }
 
