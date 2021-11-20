91b4b2785de501423c5dd553d1d0f7d88cb050e3
jooprima <jooprima@gmail.com>
2021-11-20 12:06:13 +0700

    PackageCard component

--- src/containers/Home/SecondSection/Packages/index.tsx
+++ src/containers/Home/SecondSection/Packages/index.tsx
@@ -1,3 +1,5 @@
+import PackageCard from "./PackageCard";
+
 const Packages = () => {
   return (
     <>
--- src/containers/Home/SecondSection/index.tsx
+++ src/containers/Home/SecondSection/index.tsx
@@ -4,12 +4,14 @@ import Container from "components/Container";
 import styles from "./SecondSection.module.css";
 import PromoCarousel from "./PromoCarousel";
 import "react-multi-carousel/lib/styles.css";
+import Packages from "./Packages";
 
 const SecondSection = () => {
   return (
     <section className={styles.section}>
       <Container>
         <PromoCarousel />
+        <Packages />
       </Container>
     </section>
   );
