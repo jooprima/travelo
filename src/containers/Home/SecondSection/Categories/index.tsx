55ae0d56206e579cc73141c2a771543a9519bc09
jooprima <jooprima@gmail.com>
2021-11-20 12:14:39 +0700

    Categories component

---
+++ src/containers/Home/SecondSection/Categories/index.tsx
@@ -0,0 +1,17 @@
+const Categories = () => {
+  return (
+    <>
+      <h3 className=" text-heading-3 text-gray-100 font-label font-bold pt14  mb-8">
+        Kategori Tempat
+      </h3>
+
+      <div className=" flex justify-between">
+        {Array.from({ length: 6 }).map((_, index) => (
+          <CategoryCard key={index} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default Categories;
